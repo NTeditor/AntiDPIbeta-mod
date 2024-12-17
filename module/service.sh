0024ea22a7b50e503394f1bcaf67019314688da2
NTeditor <nteditor4@gmail.com>
2024-12-17 19:40:39 +0300

    fix autostart V1

--- module/service.sh
+++ module/service.sh
@@ -1,8 +1,4 @@
 AUTOSTART="true"
-if [ -f "/system/bin/dpi" ] && [ "$AUTOSTART" == "true" ]; then
-while sleep 1; do
-if [ "$(getprop sys.boot_completed)" == "1" ]; then 
+if [ "$AUTOSTART" == "true" ]; then
 su -c "/system/bin/dpi start"  
-break; fi
-done; fi; 
-
+fi; 
